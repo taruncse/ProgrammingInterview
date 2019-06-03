1dd760c2c22fe79832db1c3404524ad84fdd85c9
Tarun <tarun.pstu@gmail.com>
2019-06-04 02:18:30 +0600

    #CTCI Tree: Height of a Binary Tree

---
+++ src/tkb/programming/interview/TreeHeight.java
@@ -0,0 +1,58 @@
+package tkb.programming.interview;
+
+import java.util.Scanner;
+
+public class TreeHeight {
+
+    // Test data
+    /*public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = height(root);
+        System.out.println(height);
+    }*/
+
+    public static int height(Node root) {
+        // Write your code here.
+        if (root == null) {
+            return -1;
+        } else {
+            return 1+Math.max(height(root.left), height(root.right));
+        }
+    }
+
+   public static Node insert(Node root, int data){
+       if (root == null){
+           return new Node(data);
+       }else {
+           Node current;
+           if (root.data >= data){
+               current = insert(root.left,data);
+               root.left = current;
+           }else {
+               current =  insert(root.right,data);
+               root.right = current;
+           }
+           return current;
+       }
+   }
+}
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
